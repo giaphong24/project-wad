fb3da8564401cc9f4f3b172f91930c98e41c03c6
giaphong24 <phongvugia17@gmail.com>
2024-06-16 15:24:32 +0700

    Add create tag function

---
+++ src/main/java/com/example/project/controller/TagController.java
@@ -0,0 +1,29 @@
+package com.example.project.controller;
+
+import com.example.project.dto.TagDto;
+import com.example.project.dto.template.ResponseTemplate;
+import com.example.project.services.TagService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1/tag")
+@RequiredArgsConstructor
+public class TagController {
+    private final TagService tagService;
+
+    @PostMapping
+    public ResponseEntity<ResponseTemplate<TagDto>> createTag(
+        @RequestBody @Valid TagDto tagDto
+    ) {
+        ResponseTemplate<TagDto> response = tagService.createTag(tagDto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+
+}
---
+++ src/main/java/com/example/project/services/impl/TagServiceImpl.java
@@ -0,0 +1,30 @@
+package com.example.project.services.impl;
+
+import com.example.project.dto.TagDto;
+import com.example.project.dto.template.ResponseTemplate;
+import com.example.project.mapper.TagMapper;
+import com.example.project.model.Tag;
+import com.example.project.repository.TagRepository;
+import com.example.project.services.TagService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class TagServiceImpl implements TagService {
+    private final TagRepository tagRepository;
+    private final TagMapper tagMapper;
+
+    @Override
+    public ResponseTemplate<TagDto> createTag(TagDto tagDto) {
+        Tag newTag = tagMapper.toTagEntity(tagDto);
+        Tag savedTag = tagRepository.save(newTag);
+
+        return ResponseTemplate.<TagDto>builder()
+            .code(HttpStatus.CREATED.value())
+            .message("Successfully created new tag!")
+            .data(tagMapper.toTagDto(savedTag))
+            .build();
+    }
+}
