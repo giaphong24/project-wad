5ff59b5aec2bebfd8135cbb8bf4959d16f26d83e
giaphong24 <phongvugia17@gmail.com>
2024-06-16 21:19:09 +0700

    Add delete tag function

--- src/main/java/com/example/project/controller/TagController.java
+++ src/main/java/com/example/project/controller/TagController.java
@@ -7,6 +7,8 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,6 +26,12 @@ public class TagController {
     ) {
         ResponseTemplate<TagDto> response = tagService.createTag(tagDto);
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<ResponseTemplate<TagDto>> deleteTag(@PathVariable Long id) {
+        ResponseTemplate<TagDto> deletedTag = tagService.deleteTag(id);
+        return ResponseEntity.ok(deletedTag);
     }
 
 }
